9a82f70cfb480db4aee16caf7e265f72c83b4197
Fasil <fasilteshsoft@gmail.com>
2024-12-17 12:46:02 +0300

    EDA techniques

---
+++ notebooks/eda.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load your dataset (replace 'your_dataset.csv' with the actual file path)
+# Ensure your file has 'headline', 'publisher', and 'publication_date' columns
+df = pd.read_csv(r'C:\Users\Dell\Desktop\Kaim\Kaim-week1\EDA\raw_analyst_ratings.csv')
